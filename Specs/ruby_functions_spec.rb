b7bf806b8f850bc4cb57f6bb831cc19068874ec6
Sam Werngren <sam.werngren@gmail.com>
2016-06-07 16:03:35 +0100

    adds multiply

--- Specs/ruby_functions_spec.rb
+++ Specs/ruby_functions_spec.rb
@@ -13,15 +13,15 @@ class Functions_Practice < MiniTest::Test
     assert_equal( 3, add_result )
   end
 
-  # def test_subtract()
-  #   subtract_result = subtract( 10, 5 )
-  #   assert_equal( 5, subtract_result )
-  # end
+  def test_subtract()
+    subtract_result = subtract( 10, 5 )
+    assert_equal( 5, subtract_result )
+  end
 
-  # def test_multiply()
-  #   multiply_result = multiply( 4, 2 )
-  #   assert_equal( 8, multiply_result )
-  # end
+  def test_multiply()
+    multiply_result = multiply( 4, 2 )
+    assert_equal( 8, multiply_result )
+  end
 
   # def test_divide()
   #   divide_result = divide( 10, 2 )
--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -5,3 +5,11 @@ end
 def add(first_number, second_number)
   return first_number + second_number
 end
+
+def subtract(first_number, second_number)
+    return first_number - second_number
+  end
+
+  def multiply(first_number, second_number)
+    return first_number * second_number
+  end
