d0984f8548555c5d58a44d38bd35a2d0c8885013
Sam Werngren <sam.werngren@gmail.com>
2016-06-07 16:32:28 +0100

    adds number to short string

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -61,6 +61,18 @@ def number_to_full_month_name(number)
   end
 end
 
+def number_to_short_month_name(number)
+  case number
+  when 1
+    return "Jan"
+  when 3
+    return "Mar"
+  when 9
+    return "Sep"
+  else
+    return "invalid number"
+  end
+end
 
 
 
