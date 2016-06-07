d4974a0b8583e32861977f409e66c99ad5f15ebd
Sam Werngren <sam.werngren@gmail.com>
2016-06-07 16:05:12 +0100

    adds divide

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -13,3 +13,7 @@ def subtract(first_number, second_number)
   def multiply(first_number, second_number)
     return first_number * second_number
   end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
